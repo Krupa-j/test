3149eb673a2013669922a7624e8628a318c5acb3
krupa-jagadeesh <krupa.jagadeesh@cbre.com>
2020-06-23 11:31:03 +0530

    Merge conflicts

--- git-training/src/test/GitTest.java
+++ git-training/src/test/GitTest.java
@@ -4,7 +4,7 @@ public class GitTest {
 
 	public static void main(String[] args) {
 		System.out.println("This is a new GIT project");
-		System.out.println("This is a cloned project");
+		System.out.println("This is a cloned project"+"cloned");
 
 	}
 
